fa92b69ae93e3b7df426d6b731ede77b9da7a8e9
Retr0-Xan <mayitey69@gmail.com>
2024-05-09 19:54:07 +0000

    -added users app name

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('',views.display_users),
-    path('register',views.register_user),
+    path('register',views.register_user, name="register"),
 ]
