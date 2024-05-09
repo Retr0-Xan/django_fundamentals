5a165b0db4367460fee468aaea66fa0df69d3f30
Retr0-Xan <mayitey69@gmail.com>
2024-05-09 12:31:59 +0000

    -added new function to display individual page views -added new path to post app urls -made post titles in posts list page a link to their individual views -changed layout template code to use template engine

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.posts_lists),
+    path('', views.posts_lists, name='posts'),
+    path('<slug:slug>', views.post_page, name='page'),
 ]
