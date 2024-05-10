74cc01c7d5d77d7e766feb51a00a2cc3b5963009
Retr0-Xan <mayitey69@gmail.com>
2024-05-10 11:59:48 +0000

    -implemented new post page -implemented log out view and function

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def posts_lists(request):
@@ -10,3 +10,8 @@ def posts_lists(request):
 def post_page(request, slug):
     post= Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post': post})
+
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('register/',views.register_user, name="register"),
     path('login/',views.log_in, name="login"),
+    path('logout/',views.log_out, name="logout"),
 ]
