edf1cf792c84d49380b6b482fc23ab730aa65983
Retr0-Xan <mayitey69@gmail.com>
2024-05-10 12:34:43 +0000

    -created a custom form for new posts -added new field in post model for post author -put new form on page, implemented validation and saving -updated posts list to show author

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,8 @@ class Post(models.Model):
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
     banner= models.ImageField(default='fallback.png', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,default=None) #on delete tells db how to handle data when user/relationship is deleted. Cascade means, if user gets
+    #deleted, all posts will be deleted.
 
     def __str__(self):
         return self.title
